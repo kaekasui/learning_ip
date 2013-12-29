60de4235d6317e83f189055960ac3e52bb323970
@kae_kasui <kaekasui_github@icloud.com>
2013-12-29 14:05:40 +0900

    refs #98 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningIp::Application.config.secret_key_base = 'daa91f760c7fa15401976850ccb9a6ad421c88e21dfb75621dd6ecbdbd611722b9aea8837490c89e84448d700996f796d0d7491c7d26e215257ae022f3e50d59'
