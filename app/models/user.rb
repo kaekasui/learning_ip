29bfefdfb2dd10630c97ff92b7d0599a53e2e085
@kae_kasui <kaekasui_github@icloud.com>
2014-01-04 06:45:03 +0900

    refs #107 modified user model for 'acts_as_paranoid' and user code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  acts_as_paranoid
+
+  before_save :set_access_code
+  
+  private
+
+  def set_access_code
+    self.code = self.code.blank? ? generate_access_code : self.code
+  end
+
+  def generate_access_code
+    code = SecureRandom.urlsafe_base64(6)
+    self.class.where(code: code).blank? ? code : generate_access_code
+  end
 end
--- db/migrate/20140103213459_add_deleted_at_and_admin_to_user.rb
+++ db/migrate/20140103213459_add_deleted_at_and_admin_to_user.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtAndAdminToUser < ActiveRecord::Migration
   def change
     add_column :users, :deleted_at, :timestamp
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
