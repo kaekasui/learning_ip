6481d648c301a7099a79de7b1a648fab8a37099c
Mayu Kojima <aquariuslake2@gmail.com>
2014-01-15 16:08:30 +0900

    refs #121 add the profile edit page

--- spec/routing/users/registrations_routing_spec.rb
+++ spec/routing/users/registrations_routing_spec.rb
@@ -5,5 +5,13 @@ describe "Users::Registration" do
     it "get '/users/sign_up' -> users/registrations#new" do
       expect(get('/users/sign_up')).to route_to('users/registrations#new')
     end
+
+    it "get '/users/profile' -> users/registrations#show" do
+      expect(get '/users/profile').to route_to('users/registrations#show')
+    end
+
+    it "get '/users/edit' -> users/registrations#edit" do
+      expect(get '/users/edit').to route_to('users/registrations#edit')
+    end
   end
 end
