5a417fd15df36bddb3b07579bb8c4ceaf61768f2
@kae_kasui <kaekasui_github@icloud.com>
2014-02-03 00:13:39 +0900

    refs #135 bug fix, twitter user updated email and password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,18 +7,47 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def edit
+    send(:"authenticate_#{resource_name}!", force: true)
+    self.resource = OriginalUser.find_by_code(current_user.code)
+    render :edit
+  end
+
+  def update
+    self.resource = resource_class.to_adapter.get!(OriginalUser.find_by_code(current_user.code).to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    updated_password = if resource.encrypted_password.blank?
+      resource.update(account_update_params)
+    else
+      resource.update_with_password(account_update_params)
+    end
+    if updated_password
+      yield resource if block_given?
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ? :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, bypass: true
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def show
     @original_user = OriginalUser.find_by_code(current_user.code) || OriginalUser.new
-    @virtual_user = VirtualUser.find_by_code(current_user.code)
+    @virtual_user = VirtualUser.where(code: current_user.code, created_at: Time.now.all_day).last
     @twitter_user = TwitterUser.find_by_code(current_user.code) || TwitterUser.new
   end
 
   def email
-    @user = VirtualUser.find_by_code(current_user.code) || VirtualUser.new
+    @user = VirtualUser.where(code: current_user.code, created_at: Time.now.all_day).last || VirtualUser.new
   end
 
   def send_email
-    @user = VirtualUser.find_by_code(current_user.code) || VirtualUser.new
+    @user = VirtualUser.where(code: current_user.code, created_at: Time.now.all_day).last || VirtualUser.new
     respond_to do |format|
       if @user.update_attributes(email: user_email_param["email"], code: current_user.code)
         NoticeMailer.change_email(@user).deliver
@@ -46,7 +75,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_email
     @virtual_user = VirtualUser.where(code: current_user.code, created_at: Time.now.all_day).first
-    @original_user = OriginalUser.find_by_code(current_user.code)
+    @original_user = OriginalUser.find_by_code(current_user.code) || OriginalUser.new(code: current_user.code)
 
     respond_to do |format|
       if user_code_param == current_user.code
@@ -84,5 +113,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def user_code_param
     params.require(:code)
+  end
+
+  protected
+  def after_update_path_for(resource)
+    users_profile_path
   end
 end
--- spec/features/twitter_user_update_profile_spec.rb
+++ spec/features/twitter_user_update_profile_spec.rb
@@ -8,10 +8,10 @@ feature 'update the profile information, and send mail.' do
     OmniAuth.config.mock_auth[provider] = omniauth_hash
 
     # login.
-    user = create(:twitter_user)
+    @user = create(:twitter_user)
     visit new_user_session_path
     click_link I18n.t("account.twitter_login")
-    login_as(user, scope: :user)
+    login_as(@user, scope: :user)
   end
 
   scenario "twitter user update the empty name." do
@@ -65,9 +65,15 @@ feature 'update the profile information, and send mail.' do
     expect(page.status_code).to eq 200
 
     # send email.
-    fill_in 'virtual_user_email', with: "abc@example.com"
+    fill_in 'virtual_user_email', with: "virtual_abc@example.com"
     click_button I18n.t("actions.mail")
     expect(page).to have_content(I18n.t("messages.send_mail"))
+
+    # update email.
+    user = VirtualUser.find_by_email("virtual_abc@example.com")
+    visit update_users_email_path(code: user.code)
+    expect(page).to have_content(I18n.t("messages.update_user_email"))
+    expect(OriginalUser.find_by_code(user.code).email).to eq "virtual_abc@example.com"
   end
 
   scenario "twitter user can't update the email with string." do
@@ -82,5 +88,68 @@ feature 'update the profile information, and send mail.' do
     expect(page.status_code).to eq 200
     url = URI.parse(page.current_url).path
     expect(url).to eq users_send_email_path
+  end
+
+  scenario "twitter user update the password with non-regist password." do
+    # display the profile page and editing email page.
+    visit users_profile_path
+    click_link 'email'
+    expect(page.status_code).to eq 200
+
+    # send email.
+    fill_in 'virtual_user_email', with: "virtual_abc@example.com"
+    click_button I18n.t("actions.mail")
+    expect(page).to have_content(I18n.t("messages.send_mail"))
+
+    # update email.
+    user = VirtualUser.find_by_email("virtual_abc@example.com")
+    visit update_users_email_path(code: user.code)
+    expect(page).to have_content(I18n.t("messages.update_user_email"))
+    expect(OriginalUser.find_by_code(user.code).email).to eq "virtual_abc@example.com"
+
+    # display the editing password page.
+    click_link "password"
+
+    # set password.
+    fill_in 'user_password', with: '12345678'
+    fill_in 'user_password_confirmation', with: '12345678'
+    click_button I18n.t("actions.update")
+    expect(page).to have_content(I18n.t("devise.registrations.updated"))
+
+    # logout.
+    click_link "logout"
+
+    # login.
+    omniauth_hash = { provider: :twitter, uid: "1234" }
+    provider = omniauth_hash[:provider]
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[provider] = omniauth_hash
+
+    click_link "login"
+    visit new_user_session_path
+    click_link I18n.t("account.twitter_login")
+    login_as(@user, scope: :user)
+
+    # update password.
+    visit users_profile_path
+    original_user = OriginalUser.find_by_code(@user.code)
+    expect(original_user.email).to eq "virtual_abc@example.com"
+    
+    click_link 'password'
+    fill_in 'user_current_password', with: '12345678'
+    fill_in 'user_password', with: '123123123'
+    fill_in 'user_password_confirmation', with: '123123123'
+    click_button I18n.t("actions.update")
+    expect(page).to have_content(I18n.t("devise.registrations.updated"))
+
+    # logout.
+    click_link "logout"
+
+    # login.
+    click_link "login"
+    fill_in 'user_email', with: original_user.email
+    fill_in 'user_password', with: '123123123'
+    click_button I18n.t("actions.login")
+    expect(page.status_code).to eq 200
   end
 end
