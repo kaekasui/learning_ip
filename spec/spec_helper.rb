9f6115066048dc88d4b68741a796aee41618653b
@kae_kasui <kaekasui_github@icloud.com>
2014-04-12 10:44:18 +0900

    refs #4 'set spec_helper for rcov'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
+equire 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'simplecov'
 
 SimpleCov.start do
   add_filter 'factories'
