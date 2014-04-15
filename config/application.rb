2c38cdaa210a84967399b13515f16654e1a1bc8f
@kae_kasui <kaekasui_github@icloud.com>
2014-04-15 09:55:57 +0900

    refs #4 'set config to generate spec files'

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,16 @@ module LearningIp
     I18n.enforce_available_locales = false
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
