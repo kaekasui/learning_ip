d0f01380829a3ed97c0ca803ee447de37965f1b0
@kae_kasui <kaekasui_github@icloud.com>
2014-04-18 13:39:54 +0900

    refs #30 'create, update and destroy sections'

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ LearningIp::Application.routes.draw do
   namespace :admin do
     root 'dashboard#index'
     resources :categories, except: [:show]
+    resources :sections
   end
 end
---
+++ spec/factories/sections.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :section, class: Section do
+    name "Section"
+  end
+end
