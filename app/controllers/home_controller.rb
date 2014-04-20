fd789fe22f760b741361302795caf181f58e830a
@kae_kasui <kaekasui_github@icloud.com>
2014-04-21 02:41:08 +0900

    refs #42 'add sections on the top'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   def index
     @post_types = PostType.display_type
     @inquiry = Inquiry.new
+    @sections = Section.limit(12)
+    @section_size = 12 / @sections.count
   end
 end
