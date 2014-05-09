0ab973b5dfa88bac131dc9ef10339156ec6be6d8
@kae_kasui <kaekasui_github@icloud.com>
2014-05-09 15:30:49 +0900

    refs #22 'add a controller spec of the dashboard'

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -1,13 +1,9 @@
 require 'spec_helper'
 
 describe Admin::DashboardController do
+  # ログインしていない場合
   context "with non-logged-in user" do
     describe '#index' do
-      it 'response code is 302.' do
-        get :index
-        expect(response.status).to eq 302
-      end
-
       it 'redirect to the top page.' do
         get :index
         expect(response).to redirect_to(root_path)
@@ -15,18 +11,33 @@ describe Admin::DashboardController do
     end
   end
 
-  context "with logged-in administrator" do
+  # ログインユーザーが一般ユーザーの場合
+  context "with a logged-in user" do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in create(:original_user, admin: false)
+    end
+
+    describe '#index' do
+      it 'redirect to the top page.' do
+        get :index
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    after do
+      sign_out :user
+    end
+  end
+
+  # ログインユーザーが管理者の場合
+  context "with a logged-in administrator" do
     before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in create(:original_user, admin: true)
     end
 
     describe '#index' do
-      it 'response code is 200.' do
-        get :index
-        expect(response.status).to eq 200
-      end
-
       it 'render template :index' do
         get :index
         expect(response).to render_template(:index)
