e7231d12e2da64a1c7009ba05703189dfbd2fd39
@kae_kasui <kaekasui_github@icloud.com>
2014-05-09 15:07:20 +0900

    refs #22 'add a routing test of the dashboard'

--- spec/routing/admin/dashboard_routing_spec.rb
+++ spec/routing/admin/dashboard_routing_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Dashboard" do
   describe "routing" do
-    it "get '/' -> admin/dashboard#index" do
+    it "get 'admin/' -> admin/dashboard#index" do
       expect(get 'admin').to route_to('admin/dashboard#index')
     end
   end
