41758d0635ebed2f9150f0241b0385330a661fa8
@kae_kasui <kaekasui_github@icloud.com>
2014-05-09 19:01:46 +0900

    refs #21 'modified a features spec of the admin menu.'

--- spec/features/admin/display_spec.rb
+++ spec/features/admin/display_spec.rb
@@ -16,8 +16,12 @@ feature 'administrators display the admin page.' do
   # 管理メニューを表示する
   scenario 'display the admin menu.' do
     visit admin_root_path
-    expect(page).to have_selector("ul.list-group > li.list-group-item > a", I18n.t("admin_menu.dashboard"))
-    expect(page).to have_selector("ul.list-group > li.list-group-item > a", I18n.t("admin_menu.category"))
-    expect(page).to have_selector("ul.list-group > li.list-group-item > a", I18n.t("admin_menu.case"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.dashboard"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.post"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.post_type"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.section"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.category"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.case"))
+    expect(page).to have_selector("ul.list-group > li.list-group-item > a", text: I18n.t("admin_menu.inquiry"))
   end
 end
